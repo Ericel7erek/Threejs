37c556845a1b91d15d15f9f6159506752263e0fa
ericel7erek <ericalaa@ymail.com>
2024-05-19 21:51:17 +0200

    RenderLoop

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+console.log(OrbitControls);
 
 const scene = new THREE.Scene()
 
@@ -10,21 +12,43 @@ const cubeMesh = new THREE.Mesh(
 cubeGeo,
 cubeMaterial
 )
-
+// The Method of adding new stuff to the scene
 scene.add(cubeMesh)
 
-const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight,0.1,30)
+const camera = new THREE.PerspectiveCamera(
+    20, 
+    window.innerWidth / window.innerHeight,
+    0.1,
+    10
+    )
 camera.position.z = 5
-
+// Not needed but you can add it(camera) to the scene
 scene.add(camera)
 
-
+//DOM container
 const canvas = document.querySelector('.threejs')
-
+//render what we have
 const renderer = new THREE.WebGLRenderer({canvas})
-renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.render(scene, camera)
 
+//How to add controls
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.autoRotate = true
+
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+})
+
+const renderloop =() => {
+    console.log("rendered");
+    controls.update()
+    renderer.render(scene, camera) // the rendering loop
+    window.requestAnimationFrame(renderloop)
+}
+renderloop()
 
 console.log(canvas);
 console.log(scene);
