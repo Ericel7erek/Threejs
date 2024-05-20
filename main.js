27c2bceaa229fa5e6e2d8caad0cc3c22176c482f
ericel7erek <ericalaa@ymail.com>
2024-05-20 02:23:58 +0200

    Reorder

--- main.js
+++ main.js
@@ -8,28 +8,44 @@ const cubeGeo = new THREE.BoxGeometry(1,1,1)
 
 const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"})
 
-const cubeMesh = new THREE.Mesh(
-cubeGeo,
-cubeMaterial
-)
+const cubeMesh = new THREE.Mesh(cubeGeo,cubeMaterial)
+cubeMesh.position.set(0,0,0)
+// cubeMesh.rotation.y = Math.PI * 2 // Default rotation
+//Easier method for calculating rotation
+cubeMesh.rotation.reorder('YXZ')
+cubeMesh.rotation.y = THREE.MathUtils.degToRad(90)
+cubeMesh.rotation.x = THREE.MathUtils.degToRad(120)
+const cubeMesh2 = new THREE.Mesh(cubeGeo,cubeMaterial)
+cubeMesh2.position.set(1.2,0,)
+console.log(cubeMesh2);
+const cubeMesh3 = new THREE.Mesh(cubeGeo,cubeMaterial)
+cubeMesh3.position.set(2.4,0,0)
+const group = new THREE.Group
+group.add(cubeMesh)
+// group.add(cubeMesh2)
+// group.add(cubeMesh3)
 // The Method of adding new stuff to the scene
-scene.add(cubeMesh)
-cubeMesh.position.y = 0.5
-cubeMesh.position.x = -1
-cubeMesh.position.z = -1
+scene.add(group)
+// const tempVector = new THREE.Vector3(0,3,0)
+// cubeMesh.position.y = 0.5
+// cubeMesh.position.x = -1
+// cubeMesh.position.z = -1
+// cubeMesh.position.copy(tempVector)
 
 const axesHelper = new THREE.AxesHelper(1)
-scene.add(axesHelper)
+cubeMesh.add(axesHelper)
 
 const camera = new THREE.PerspectiveCamera(
     20, 
     window.innerWidth / window.innerHeight,
     0.1,
-    10
+    200
     )
 camera.position.z = 5
 // Not needed but you can add it(camera) to the scene
 scene.add(camera)
+console.log(cubeMesh.position.distanceTo(camera.position));
+//use .set for changing 3 values at the same time
 
 //DOM container
 const canvas = document.querySelector('.threejs')
@@ -42,7 +58,7 @@ const renderer = new THREE.WebGLRenderer({
 
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
-controls.autoRotate = true
+// controls.autoRotate = true
 
 window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight
